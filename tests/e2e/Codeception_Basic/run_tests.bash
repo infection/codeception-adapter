1cb6d76adc4ba0bd11adf9432bca41e91bc980f7
borNfreee <b0rn@list.ru>
2020-03-18 17:12:30 +0300

    Run e2e tests. Run the very latest version of adapter for e2e test

---
+++ tests/e2e/Codeception_Basic/run_tests.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+tputx () {
+	test -x $(which tput) && tput "$@"
+}
+
+run () {
+    local INFECTION=${1}
+    local PHPARGS=${2}
+
+    if [ "$DRIVER" = "phpdbg" ]
+    then
+        phpdbg $PHPARGS -qrr $INFECTION
+    else
+        php $PHPARGS $INFECTION
+    fi
+}
+
+
+cd "$(dirname "$0")"
+
+rm -rf codeception-package
+mkdir codeception-package
+
+git_branch="${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
+
+if [ "$git_branch" == "master" ]; then
+  exit 0;
+fi;
+
+sed -i "s/\"infection\/codeception-adapter\": \"dev-master\"/\"infection\/codeception-adapter\": \"dev-${git_branch}\"/" composer.json
+
+cp -r ../../../src codeception-package/src
+cp ../../../composer.json codeception-package
+
+set -e pipefail
+
+rm -f composer.lock
+composer install
+
+run "vendor/bin/infection"
+
+git checkout composer.json
+
+diff -w expected-output.txt infection.log
+
