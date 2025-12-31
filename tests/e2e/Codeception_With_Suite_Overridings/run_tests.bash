f3ff88fb76bd0a58e9bffcbb3c85b649dc29a37f
Théo FIDRY <theo.fidry@gmail.com>
2025-12-31 17:35:04 +0100

    try to simplify the script

--- tests/e2e/Codeception_With_Suite_Overridings/run_tests.bash
+++ tests/e2e/Codeception_With_Suite_Overridings/run_tests.bash
@@ -32,21 +32,11 @@ then
     exit 0
 fi
 
-git_branch=$(echo "${GITHUB_HEAD_REF:-$(git rev-parse --abbrev-ref HEAD)}" | sed 's/\//\\\//g')
-
-if [ "$git_branch" == "master" ]; then
-  exit 0;
-fi;
-
-sed -i "s/\"infection\/codeception-adapter\": \"dev-master\"/\"infection\/codeception-adapter\": \"dev-${git_branch}\"/" composer.json
-
 set -e pipefail
 
 rm -f composer.lock
 composer install
 
 run "vendor/bin/infection"
-
-git checkout composer.json
 
 diff -w expected-output.txt infection.log
