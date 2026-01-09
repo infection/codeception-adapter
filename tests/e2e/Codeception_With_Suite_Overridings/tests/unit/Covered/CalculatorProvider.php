8190168a5593ec59fc839c7d5f9090ce80502123
Théo FIDRY <theo.fidry@gmail.com>
2026-01-09 13:54:53 +0100

    test: Add more tests for a more complete coverage sample

---
+++ tests/e2e/Codeception_With_Suite_Overridings/tests/unit/Covered/CalculatorProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Codeception_With_Suite_Overridings\Tests\unit\Covered;
+
+final class CalculatorProvider
+{
+    public static function provideAdditions(): iterable
+    {
+        yield [2, 3, 5];
+        yield [-5, 5, 0];
+        yield 'with a key' => [-3, -7, -10];
+        yield 'with a key with (\'"#::&|) special characters' => [-5, -5, -10];
+    }
+}
