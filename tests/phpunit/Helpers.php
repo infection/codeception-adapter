e7fca5f3e8f7107473c1fdf225b5d8c0a10c9024
Théo FIDRY <theo.fidry@gmail.com>
2026-04-21 11:23:47 +0200

    update

--- tests/phpunit/Helpers.php
+++ tests/phpunit/Helpers.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This code is licensed under the BSD 3-Clause License.
  *
@@ -30,8 +31,6 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-declare(strict_types=1);
 
 namespace Infection\Tests\TestFramework\Codeception;
 
